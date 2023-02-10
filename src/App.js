f101b762919604f2ca3366b81a05433ddeec6665
Tarcísio Batista de Freitas Junior <tarcisiodf23@gmail.com>
2023-02-10 17:40:55 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <p> 
+            Testando
+          </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
